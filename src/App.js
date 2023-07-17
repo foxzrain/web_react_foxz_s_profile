ecb28d5a57c7e4457145c2aed594d1fbe89ccf37
Pichaya Deachpol <pichaya@sic.co.th>
2023-07-17 13:05:26 +0700

    - profile picture - menu icon

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React, { Component } from "react";
-import ReactGA from "react-ga";
 import $ from "jquery";
 import "./App.css";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import About from "./Components/About";
 import Resume from "./Components/Resume";
-import Contact from "./Components/Contact";
 import Portfolio from "./Components/Portfolio";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      foo: "bar",
       resumeData: {}
     };
-
-    ReactGA.initialize("UA-110570651-1");
-    ReactGA.pageview(window.location.pathname);
   }
 
   getResumeData() {
@@ -29,7 +23,7 @@ class App extends Component {
       success: function(data) {
         this.setState({ resumeData: data });
       }.bind(this),
-      error: function(xhr, status, err) {
+      error: function(err) {
         console.log(err);
         alert(err);
       }
@@ -47,7 +41,6 @@ class App extends Component {
         <About data={this.state.resumeData.main} />
         <Resume data={this.state.resumeData.resume} />
         <Portfolio data={this.state.resumeData.portfolio} />
-        <Contact data={this.state.resumeData.main} />
         <Footer data={this.state.resumeData.main} />
       </div>
     );
